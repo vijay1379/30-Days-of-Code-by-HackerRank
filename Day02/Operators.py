536dac8f55cf0bac37070aedaf85a1f4593d6578
Vijay Kumar N <117030617+vijay1379@users.noreply.github.com>
2023-12-09 23:39:55 +0530

    Create Operators.py

---
+++ Day02/Operators.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+def solve(meal_cost, tip_percent, tax_percent):
+    tip_percent=(tip_percent/100)*meal_cost
+    tax_percent=(tax_percent/100)*meal_cost
+    meal_cost=meal_cost+tip_percent+tax_percent
+    print(round(meal_cost))
+
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+    tip_percent = int(input().strip())
+    tax_percent = int(input().strip())
+    solve(meal_cost, tip_percent, tax_percent)
