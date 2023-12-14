cc6782b17b827abab10c6db49b7293476ef16538
Vijay Kumar N <117030617+vijay1379@users.noreply.github.com>
2023-12-14 21:16:27 +0530

    Create Arrays.py

---
+++ Day07/Arrays.py
@@ -0,0 +1,12 @@
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    rev=arr[::-1]
+    for i in range(0,n):
+        print(rev[i], end=" ")
